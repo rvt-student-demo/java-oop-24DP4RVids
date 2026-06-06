749b8b5687a72c237381ed3729582750069f547e
Ricards Vids <A240495RV@rvt.lv>
2026-06-06 21:26:31 +0300

    I Owe You uzdevums

---
+++ Online_Shop/Item.java
@@ -0,0 +1,29 @@
+public class Item {
+
+    private String product;
+    private int qty;
+    private int unitPrice;
+
+    public Item(String product, int qty, int unitPrice) {
+        this.product = product;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public int price() {
+        return qty * unitPrice;
+    }
+
+    public void increaseQuantity() {
+        this.qty++;
+    }
+
+    @Override
+    public String toString() {
+        return product + ": " + qty;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+}
